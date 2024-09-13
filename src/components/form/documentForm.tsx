b269ada59c1433de561741f9e75a94d101caa9d1
Danil <daniltro1@yandex.ru>
2024-09-13 21:54:05 +0300

    added validation form inputs

--- src/components/form/documentForm.tsx
+++ src/components/form/documentForm.tsx
@@ -1,3 +1,186 @@
+// import React, { useEffect, useState } from "react";
+// import {
+//   Dialog,
+//   DialogActions,
+//   DialogContent,
+//   DialogTitle,
+//   TextField,
+//   Button,
+//   Typography, // Используем Typography для стилизации текста
+// } from "@mui/material";
+// import { useDispatch } from "../../services/store";
+// import { IAddedDocument, IDocument } from "../../types/types";
+// import { addDataItem, updateDataItem } from "../../services/actions/actions";
+// import { formatDate } from "../../utils/utils";
+// import styles from "./documentForm.module.css";
+
+// interface DocumentFormProps {
+//   onClose: () => void;
+//   initialData?: IDocument | null;
+// }
+
+// const DocumentForm: React.FC<DocumentFormProps> = ({
+//   onClose,
+//   initialData,
+// }) => {
+//   const dispatch = useDispatch();
+
+//   const [formData, setFormData] = useState<IAddedDocument>({
+//     documentStatus: "",
+//     employeeNumber: "",
+//     documentType: "",
+//     documentName: "",
+//     companySignatureName: "",
+//     employeeSignatureName: "",
+//     employeeSigDate: "",
+//     companySigDate: "",
+//   });
+//   const [formErrors, setFormErrors] = useState<Record<string, string>>({});
+
+//   useEffect(() => {
+//     if (initialData) {
+//       // Заполняем форму данными для редактирования
+//       setFormData({
+//         documentStatus: initialData.documentStatus,
+//         employeeNumber: initialData.employeeNumber,
+//         documentType: initialData.documentType,
+//         documentName: initialData.documentName,
+//         companySignatureName: initialData.companySignatureName,
+//         employeeSignatureName: initialData.employeeSignatureName,
+//         employeeSigDate: formatDate(initialData.employeeSigDate),
+//         companySigDate: formatDate(initialData.companySigDate),
+//       });
+//     }
+//   }, [initialData]);
+
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+//   const handleSubmit = () => {
+//     const token = localStorage.getItem("authToken");
+//     if (token) {
+//       if (initialData) {
+//         // Редактирование существующего документа
+//         dispatch(
+//           updateDataItem({
+//             id: initialData.id,
+//             editItem: { ...formData, id: initialData.id },
+//             token,
+//           })
+//         );
+//       } else {
+//         // Создание нового документа
+//         dispatch(addDataItem({ newItem: formData, token }));
+//       }
+//       onClose();
+//     }
+//   };
+
+//   const isFormValid = () => {
+//     return Object.values(formData).every((value) => value.trim() !== "");
+//   };
+
+//   return (
+//     <Dialog open onClose={onClose}>
+//       <DialogTitle>
+//         <Typography>
+//           {initialData ? "Редактировать документ" : "Добавить новый документ"}
+//         </Typography>
+//       </DialogTitle>
+//       <DialogContent>
+//         <TextField
+//           margin="dense"
+//           name="documentStatus"
+//           label="Статус документа"
+//           fullWidth
+//           value={formData.documentStatus}
+//           onChange={handleChange}
+//         />
+//         <TextField
+//           margin="dense"
+//           name="employeeNumber"
+//           label="Номер работника"
+//           fullWidth
+//           value={formData.employeeNumber}
+//           onChange={handleChange}
+//         />
+//         <TextField
+//           margin="dense"
+//           name="documentType"
+//           label="Тип документа"
+//           fullWidth
+//           value={formData.documentType}
+//           onChange={handleChange}
+//         />
+//         <TextField
+//           margin="dense"
+//           name="documentName"
+//           label="Название документа"
+//           fullWidth
+//           value={formData.documentName}
+//           onChange={handleChange}
+//         />
+//         <TextField
+//           margin="dense"
+//           name="companySignatureName"
+//           label="Подпись компании"
+//           fullWidth
+//           value={formData.companySignatureName}
+//           onChange={handleChange}
+//         />
+//         <TextField
+//           margin="dense"
+//           name="employeeSignatureName"
+//           label="Подпись работника"
+//           fullWidth
+//           value={formData.employeeSignatureName}
+//           onChange={handleChange}
+//         />
+//         <TextField
+//           margin="dense"
+//           name="employeeSigDate"
+//           label="Дата подписания работником"
+//           type="date"
+//           fullWidth
+//           InputLabelProps={{ shrink: true }}
+//           value={formData.employeeSigDate}
+//           onChange={handleChange}
+//         />
+//         <TextField
+//           margin="dense"
+//           name="companySigDate"
+//           label="Дата подписания компанией"
+//           type="date"
+//           fullWidth
+//           InputLabelProps={{ shrink: true }}
+//           value={formData.companySigDate}
+//           onChange={handleChange}
+//         />
+//       </DialogContent>
+//       <DialogActions className={styles.buttonBox}>
+//         <Button variant="contained" onClick={onClose} color="primary">
+//           Отмена
+//         </Button>
+
+//         <Button
+//           disabled={!isFormValid()}
+//           variant="contained"
+//           onClick={handleSubmit}
+//           color="primary"
+//         >
+//           Сохранить
+//         </Button>
+//       </DialogActions>
+//     </Dialog>
+//   );
+// };
+
+// export default DocumentForm;
+
 import React, { useEffect, useState } from "react";
 import {
   Dialog,
@@ -6,12 +189,14 @@ import {
   DialogTitle,
   TextField,
   Button,
+  Typography,
 } from "@mui/material";
 import { useDispatch } from "../../services/store";
 import { IAddedDocument, IDocument } from "../../types/types";
 import { addDataItem, updateDataItem } from "../../services/actions/actions";
 import { formatDate } from "../../utils/utils";
 import styles from "./documentForm.module.css";
+
 interface DocumentFormProps {
   onClose: () => void;
   initialData?: IDocument | null;
@@ -34,9 +219,10 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
     companySigDate: "",
   });
 
+  const [formErrors, setFormErrors] = useState<{ [key: string]: string }>({});
+
   useEffect(() => {
     if (initialData) {
-      // Заполняем форму данными для редактирования
       setFormData({
         documentStatus: initialData.documentStatus,
         employeeNumber: initialData.employeeNumber,
@@ -51,17 +237,36 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
   }, [initialData]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
+    // Убираем ошибку при изменении значения
+    setFormErrors((errors) => ({
+      ...errors,
+      [name]: value.trim() ? "" : "Это поле обязательно для заполнения",
+    }));
   };
 
   const handleSubmit = () => {
+    const errors: { [key: string]: string } = {};
+
+    // Проверяем все поля на заполненность
+    Object.keys(formData).forEach((key) => {
+      if (!formData[key as keyof IAddedDocument].trim()) {
+        errors[key] = "Это поле обязательно для заполнения";
+      }
+    });
+
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      return;
+    }
+
     const token = localStorage.getItem("authToken");
     if (token) {
       if (initialData) {
-        // Редактирование существующего документа
         dispatch(
           updateDataItem({
             id: initialData.id,
@@ -70,7 +275,6 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           })
         );
       } else {
-        // Создание нового документа
         dispatch(addDataItem({ newItem: formData, token }));
       }
       onClose();
@@ -84,7 +288,9 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
   return (
     <Dialog open onClose={onClose}>
       <DialogTitle>
-        <h3 className={styles.dialogTitle}>Добавить новый документ</h3>
+        <Typography>
+          {initialData ? "Редактировать документ" : "Добавить новый документ"}
+        </Typography>
       </DialogTitle>
       <DialogContent>
         <TextField
@@ -94,6 +300,8 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           fullWidth
           value={formData.documentStatus}
           onChange={handleChange}
+          error={!!formErrors.documentStatus}
+          helperText={formErrors.documentStatus}
         />
         <TextField
           margin="dense"
@@ -102,6 +310,8 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           fullWidth
           value={formData.employeeNumber}
           onChange={handleChange}
+          error={!!formErrors.employeeNumber}
+          helperText={formErrors.employeeNumber}
         />
         <TextField
           margin="dense"
@@ -110,6 +320,8 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           fullWidth
           value={formData.documentType}
           onChange={handleChange}
+          error={!!formErrors.documentType}
+          helperText={formErrors.documentType}
         />
         <TextField
           margin="dense"
@@ -118,6 +330,8 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           fullWidth
           value={formData.documentName}
           onChange={handleChange}
+          error={!!formErrors.documentName}
+          helperText={formErrors.documentName}
         />
         <TextField
           margin="dense"
@@ -126,6 +340,8 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           fullWidth
           value={formData.companySignatureName}
           onChange={handleChange}
+          error={!!formErrors.companySignatureName}
+          helperText={formErrors.companySignatureName}
         />
         <TextField
           margin="dense"
@@ -134,6 +350,8 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           fullWidth
           value={formData.employeeSignatureName}
           onChange={handleChange}
+          error={!!formErrors.employeeSignatureName}
+          helperText={formErrors.employeeSignatureName}
         />
         <TextField
           margin="dense"
@@ -144,6 +362,8 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           InputLabelProps={{ shrink: true }}
           value={formData.employeeSigDate}
           onChange={handleChange}
+          error={!!formErrors.employeeSigDate}
+          helperText={formErrors.employeeSigDate}
         />
         <TextField
           margin="dense"
@@ -154,9 +374,11 @@ const DocumentForm: React.FC<DocumentFormProps> = ({
           InputLabelProps={{ shrink: true }}
           value={formData.companySigDate}
           onChange={handleChange}
+          error={!!formErrors.companySigDate}
+          helperText={formErrors.companySigDate}
         />
       </DialogContent>
-      <DialogActions className={styles.buttonBox} MuiDialogActions-root>
+      <DialogActions className={styles.buttonBox}>
         <Button variant="contained" onClick={onClose} color="primary">
           Отмена
         </Button>
