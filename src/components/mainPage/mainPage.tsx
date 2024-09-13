7769fa65c0cf16a3d9f330cf21477d98a0654f32
Danil <daniltro1@yandex.ru>
2024-09-13 20:22:54 +0300

    add mobile view

--- src/components/mainPage/mainPage.tsx
+++ src/components/mainPage/mainPage.tsx
@@ -1,17 +1,192 @@
+// import React, { useEffect, useState } from "react";
+// import {
+//   Box,
+//   Button,
+//   CircularProgress,
+//   Paper,
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableContainer,
+//   TableHead,
+//   TableRow,
+//   Typography,
+// } from "@mui/material";
+// import { useDispatch, useSelector } from "../../services/store";
+// import {
+//   selectAllDocuments,
+//   selectDocumentsError,
+//   selectDocumentsLoading,
+// } from "../../services/slices/dataSlice";
+// import DocumentForm from "../form/documentForm";
+// import { IDocument } from "../../types/types";
+// import { fetchData, removeDataItem } from "../../services/actions/actions";
+// import { formatDate } from "../../utils/utils";
+// import styles from "./mainPage.module.css";
+// import { useNavigate } from "react-router-dom";
+
+// export const MainPage: React.FC = () => {
+//   const dispatch = useDispatch();
+//   const data = useSelector(selectAllDocuments);
+//   const loading = useSelector(selectDocumentsLoading);
+//   const error = useSelector(selectDocumentsError);
+//   const navigate = useNavigate();
+
+//   const [isFormOpen, setIsFormOpen] = useState(false);
+//   const [currentDocument, setCurrentDocument] = useState<IDocument | null>(
+//     null
+//   );
+
+//   useEffect(() => {
+//     const token = localStorage.getItem("authToken");
+
+//     if (!token) {
+//       // Если токена нет, перенаправляем на страницу входа
+//       navigate("/login");
+//     } else {
+//       // Если токен есть, загружаем данные
+//       dispatch(fetchData(token));
+//     }
+//   }, [dispatch, navigate]);
+
+//   const handleDelete = (id: string) => {
+//     const token = localStorage.getItem("authToken");
+//     if (token) {
+//       dispatch(removeDataItem({ id, token })); // Передаем id и token
+//     }
+//   };
+
+//   const handleEdit = (item: IDocument) => {
+//     setCurrentDocument(item); // Устанавливаем текущий документ для редактирования
+//     setIsFormOpen(true); // Открываем форму
+//   };
+
+//   const handleOpenForm = () => {
+//     setCurrentDocument(null);
+//     setIsFormOpen(true); // Открыть форму
+//   };
+//   const handleCloseForm = () => setIsFormOpen(false);
+
+//   return (
+//     <div className={styles.container}>
+//       {error && typeof error === "string" && (
+//         <Typography color="error">{error}</Typography>
+//       )}
+
+//       {loading ? (
+//         <Box className={styles.spinnerBox}>
+//           <CircularProgress />
+//         </Box>
+//       ) : (
+//         <div className={styles.tableContainer}>
+//           <Table className={styles.table}>
+//             <TableHead className={styles.tableHead}>
+//               <TableRow className={styles.tableRow}>
+//                 <TableCell className={styles.tableCell}>
+//                   Статус документа
+//                 </TableCell>
+//                 <TableCell className={styles.tableCell}>
+//                   Номер работника
+//                 </TableCell>
+//                 <TableCell className={styles.tableCell}>
+//                   Тип документа
+//                 </TableCell>
+//                 <TableCell className={styles.tableCell}>
+//                   Название документа
+//                 </TableCell>
+//                 <TableCell className={styles.tableCell}>
+//                   Подпись компании
+//                 </TableCell>
+//                 <TableCell className={styles.tableCell}>
+//                   Подпись работника
+//                 </TableCell>
+//                 <TableCell className={styles.tableCell}>
+//                   Дата подписания работником
+//                 </TableCell>
+//                 <TableCell className={styles.tableCell}>
+//                   Дата подписания компанией
+//                 </TableCell>
+//                 <TableCell className={styles.tableCell} colSpan={2}>
+//                   <Box className={styles.buttonBox}>
+//                     <Button
+//                       variant="contained"
+//                       color="primary"
+//                       onClick={handleOpenForm}
+//                     >
+//                       Добавить новый документ
+//                     </Button>
+//                   </Box>
+//                 </TableCell>
+//               </TableRow>
+//             </TableHead>
+//             <TableBody className={styles.tableBody}>
+//               {Array.isArray(data) && data.length > 0 ? (
+//                 data.map((item) => (
+//                   <TableRow key={item.id} className={styles.tableRow}>
+//                     <TableCell className={styles.tableCell}>
+//                       {item.documentStatus}
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       {item.employeeNumber}
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       {item.documentType}
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       {item.documentName}
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       {item.companySignatureName}
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       {item.employeeSignatureName}
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       {formatDate(item.employeeSigDate)}
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       {formatDate(item.companySigDate)}
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       <Button
+//                         variant="contained"
+//                         color="primary"
+//                         onClick={() => handleDelete(item.id)}
+//                       >
+//                         Удалить
+//                       </Button>
+//                     </TableCell>
+//                     <TableCell className={styles.tableCell}>
+//                       <Button
+//                         variant="contained"
+//                         color="primary"
+//                         onClick={() => handleEdit(item)}
+//                       >
+//                         Редактировать
+//                       </Button>
+//                     </TableCell>
+//                   </TableRow>
+//                 ))
+//               ) : (
+//                 <TableRow>
+//                   <TableCell colSpan={8}>Нет данных для отображения</TableCell>
+//                 </TableRow>
+//               )}
+//             </TableBody>
+//           </Table>
+//         </div>
+//       )}
+//       {isFormOpen && (
+//         <DocumentForm onClose={handleCloseForm} initialData={currentDocument} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MainPage;
+
 import React, { useEffect, useState } from "react";
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "../../services/store";
 import {
   selectAllDocuments,
@@ -41,10 +216,8 @@ export const MainPage: React.FC = () => {
     const token = localStorage.getItem("authToken");
 
     if (!token) {
-      // Если токена нет, перенаправляем на страницу входа
       navigate("/login");
     } else {
-      // Если токен есть, загружаем данные
       dispatch(fetchData(token));
     }
   }, [dispatch, navigate]);
@@ -52,19 +225,20 @@ export const MainPage: React.FC = () => {
   const handleDelete = (id: string) => {
     const token = localStorage.getItem("authToken");
     if (token) {
-      dispatch(removeDataItem({ id, token })); // Передаем id и token
+      dispatch(removeDataItem({ id, token }));
     }
   };
 
   const handleEdit = (item: IDocument) => {
-    setCurrentDocument(item); // Устанавливаем текущий документ для редактирования
-    setIsFormOpen(true); // Открываем форму
+    setCurrentDocument(item);
+    setIsFormOpen(true);
   };
 
   const handleOpenForm = () => {
     setCurrentDocument(null);
-    setIsFormOpen(true); // Открыть форму
+    setIsFormOpen(true);
   };
+
   const handleCloseForm = () => setIsFormOpen(false);
 
   return (
@@ -78,103 +252,70 @@ export const MainPage: React.FC = () => {
           <CircularProgress />
         </Box>
       ) : (
-        <TableContainer className={styles.tableContainer} component={Paper}>
-          <Table className={styles.table}>
-            <TableHead className={styles.tableHead}>
-              <TableRow className={styles.tableRow}>
-                <TableCell className={styles.tableCell}>
-                  Статус документа
-                </TableCell>
-                <TableCell className={styles.tableCell}>
-                  Номер работника
-                </TableCell>
-                <TableCell className={styles.tableCell}>
-                  Тип документа
-                </TableCell>
-                <TableCell className={styles.tableCell}>
-                  Название документа
-                </TableCell>
-                <TableCell className={styles.tableCell}>
-                  Подпись компании
-                </TableCell>
-                <TableCell className={styles.tableCell}>
-                  Подпись работника
-                </TableCell>
-                <TableCell className={styles.tableCell}>
-                  Дата подписания работником
-                </TableCell>
-                <TableCell className={styles.tableCell}>
-                  Дата подписания компанией
-                </TableCell>
-                <TableCell className={styles.tableCell} colSpan={2}>
+        <Box className={styles.flexContainer}>
+          <Box className={styles.headerButtonBox}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleOpenForm}
+            >
+              Добавить новый документ
+            </Button>
+          </Box>
+          <Box className={styles.documentContainer}>
+            {Array.isArray(data) && data.length > 0 ? (
+              data.map((item) => (
+                <Box key={item.id} className={styles.documentItem}>
+                  <Typography className={styles.documentOption} variant="body2">
+                    Статус документа: {item.documentStatus}
+                  </Typography>
+                  <Typography className={styles.documentOption} variant="body2">
+                    Номер работника: {item.employeeNumber}
+                  </Typography>
+                  <Typography className={styles.documentOption} variant="body2">
+                    Тип документа: {item.documentType}
+                  </Typography>
+                  <Typography className={styles.documentOption} variant="body2">
+                    Название документа: {item.documentName}
+                  </Typography>
+                  <Typography className={styles.documentOption} variant="body2">
+                    Подпись компании: {item.companySignatureName}
+                  </Typography>
+                  <Typography className={styles.documentOption} variant="body2">
+                    Подпись работника: {item.employeeSignatureName}
+                  </Typography>
+                  <Typography className={styles.documentOption} variant="body2">
+                    Дата подписания работником:{" "}
+                    {formatDate(item.employeeSigDate)}
+                  </Typography>
+                  <Typography className={styles.documentOption} variant="body2">
+                    Дата подписания компанией: {formatDate(item.companySigDate)}
+                  </Typography>
                   <Box className={styles.buttonBox}>
                     <Button
                       variant="contained"
                       color="primary"
-                      onClick={handleOpenForm}
+                      onClick={() => handleDelete(item.id)}
                     >
-                      Добавить новый документ
+                      Удалить
                     </Button>
                   </Box>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody className={styles.tableBody}>
-              {Array.isArray(data) && data.length > 0 ? (
-                data.map((item) => (
-                  <TableRow key={item.id} className={styles.tableRow}>
-                    <TableCell className={styles.tableCell}>
-                      {item.documentStatus}
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      {item.employeeNumber}
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      {item.documentType}
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      {item.documentName}
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      {item.companySignatureName}
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      {item.employeeSignatureName}
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      {formatDate(item.employeeSigDate)}
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      {formatDate(item.companySigDate)}
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => handleDelete(item.id)}
-                      >
-                        Удалить
-                      </Button>
-                    </TableCell>
-                    <TableCell className={styles.tableCell}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => handleEdit(item)}
-                      >
-                        Редактировать
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={8}>Нет данных для отображения</TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
+                  <Box className={styles.buttonBox}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => handleEdit(item)}
+                    >
+                      Редактировать
+                    </Button>
+                  </Box>
+                </Box>
+              ))
+            ) : (
+              <Typography>Нет данных для отображения</Typography>
+            )}
+          </Box>
+        </Box>
       )}
       {isFormOpen && (
         <DocumentForm onClose={handleCloseForm} initialData={currentDocument} />
--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    mode: 'light', // или 'dark'
+    mode: "light", // или 'dark'
     primary: {
       main: "#1976d2",
       light: "#63a4ff",
@@ -29,7 +29,59 @@ const theme = createTheme({
       main: "#4caf50",
     },
   },
+  typography: {
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    h1: {
+      fontSize: "2.5rem",
+      fontWeight: 300,
+      letterSpacing: "-0.01562em",
+    },
+    h2: {
+      fontSize: "2rem",
+      fontWeight: 300,
+      letterSpacing: "-0.00833em",
+    },
+    h3: {
+      fontSize: "1.75rem",
+      fontWeight: 400,
+    },
+    h4: {
+      fontSize: "1.5rem",
+      fontWeight: 400,
+    },
+    h5: {
+      fontSize: "1.25rem",
+      fontWeight: 400,
+    },
+    h6: {
+      fontSize: "1rem",
+      fontWeight: 500,
+    },
+    body1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+    },
+    body2: {
+      fontSize: "1.25rem",
+      fontWeight: 400,
+    },
+    subtitle1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+    },
+    subtitle2: {
+      fontSize: "0.875rem",
+      fontWeight: 500,
+    },
+    caption: {
+      fontSize: "0.75rem",
+      fontWeight: 400,
+    },
+    overline: {
+      fontSize: "0.75rem",
+      fontWeight: 400,
+    },
+  },
 });
-
 
 export default theme;
